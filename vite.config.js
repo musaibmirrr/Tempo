745ebf799b39f1c5b74f912eeac9ac221949421a
Musiab <musaibjeelani999@gmail.com>
2025-02-16 20:32:47 +0530

    config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base : '/Tempo',
-  server : {
-    historyApiFallback : true
+  build: {
+    outDir: 'dist',
   }
 })
